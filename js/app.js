c3e312b21fc0183ba1be6c8ffb6f20236190c659
RosaCasa <rosamari.sab@gmail.com>
2020-09-30 23:49:05 +0200

    Acabado tablero básico y empezando class Jugador

---
+++ js/app.js
@@ -0,0 +1,19 @@
+let player1 = "";
+let player2 = "";
+let player3 = "";
+let player4 = "";
+
+class Jugador {
+
+    constructor (nombre, dinero, propiedades, cartas, imagen) {
+
+        this.nombre = nombre;
+        this.dinero = dinero;
+        this.propiedades = propiedades;
+        this.cartas = cartas;
+        
+        this.imagen = imagen;
+    }
+
+}
+
